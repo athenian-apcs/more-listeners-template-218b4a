5377b13e19f64b1e7d812e204e972a1777a21492
Nathan Lin <nathanlukelin@gmail.com>
2022-02-24 07:48:18 -0800

    Initial commit

---
+++ src/main/java/Quadratic.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Quadratic {
+    public Quadratic() {
+        // Set up the frame
+        JFrame frame = new JFrame("Quadratic Formula");
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set up and add the panels
+        JPanel formulaPanel = new JPanel();
+        LTPanel aPanel = new LTPanel("a =", 10);
+        LTPanel bPanel = new LTPanel("b =", 10);
+        LTPanel cPanel = new LTPanel("c =", 10);
+        JPanel buttonPanel = new JPanel();
+        LTPanel answerPanel = new LTPanel("Answer:", 10);
+
+        frame.add(formulaPanel);
+        frame.add(aPanel);
+        frame.add(bPanel);
+        frame.add(cPanel);
+        frame.add(buttonPanel);
+        frame.add(answerPanel);
+
+        // Set up individual panels
+        // formulaPanel code
+        JLabel formulaLabel = new JLabel("ax^2 + bx + c = 0");
+        formulaPanel.add(formulaLabel);
+
+        // buttonPanel code
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                aPanel.setText("");
+                bPanel.setText("");
+                cPanel.setText("");
+            }
+        });
+
+        JButton calculateButton = new JButton("Calculate");
+        calculateButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                // Potentially a lot of code here
+                // But just add code to call quadForm
+
+                // YOUR CODE HERE
+            }
+        });
+
+        buttonPanel.add(clearButton);
+        buttonPanel.add(calculateButton);
+
+        // pack and show
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void quadForm() {
+        // YOUR CODE HERE
+    }
+
+    public static void main(String[] args) {
+        Quadratic q = new Quadratic();
+    }
+}
---
+++ src/main/java/SimpleClassGUI.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleClassGUI {
+    public JFrame frame;
+    public JTextField field1;
+    public JButton button1, button2;
+
+    public SimpleClassGUI() {
+        // Set up the frame
+        frame = new JFrame("Simple Example");
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Need to create this before button ActionListener code
+        field1 = new JTextField();
+        button1 = new JButton("Clear");
+        button2 = new JButton("Do Something");
+
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(field1);
+
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                field1.setText("");
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                method1();
+            }
+        });
+
+        // pack and show
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    // The code for this method cannot work
+    // because we only have local variables
+    public void method1() {
+        field1.setText("Hi");
+    }
+
+    public static void main(String[] args) {
+        SimpleClassGUI s = new SimpleClassGUI();
+    }
+}
