6eaf71e0ed70d42efa2857671ac3745713a7222b
sthompson-athenian <sthompson@athenian.org>
2025-03-03 12:26:13 -0800

    Update SudokuButton to stop using the weird "implements KeyListener" setup

--- src/main/java/SudokuButton.java
+++ src/main/java/SudokuButton.java
@@ -2,39 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import static java.awt.event.KeyEvent.*;
 
 
-public class SudokuButton extends JButton implements KeyListener {
+public class SudokuButton extends JButton {
     public SudokuButton() {
-        super(" ");
-        this.addKeyListener(this);
+        this(" ");
     }
-    
+
     public SudokuButton(String label) {
         super(label);
-        this.addKeyListener(this);
-    }
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // To interact with the current SudokuButton, use: this
+                // For instance, if we wanted to get the text of the
+                // current button, we'd say: this.getText()
+            }
 
-    // when the key goes down
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // To interact with the current SudokuButton, use: this
-        // For instance, if we wanted to get the text of the
-        // current button, we'd say: this.getText()
-    }
+            // Leave this blank
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
 
-    // when the key goes up
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    // keyTyped only works for characters that can be typed (like letters)
-    // so we don't use it often
-    @Override
-    public void keyTyped(KeyEvent e) {
-
+            // Leave this blank
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+        });
     }
 
     // Don't worry about this, this just changes the minimum size of a button
